24605f4e8722f25b02a128f0fd186e4d27c32f89
Humberto Domínguez <humbertodelac13@gmail.com>
2024-03-13 20:25:32 -0600

    primer commit

---
+++ src/main/java/com/practica/Heladas/HeladasApplication.java
@@ -0,0 +1,13 @@
+package com.practica.Heladas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeladasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeladasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practica/Heladas/HeladasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.Heladas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeladasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
